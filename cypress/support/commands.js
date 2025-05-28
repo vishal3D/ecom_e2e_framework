26d3882b7f7a127e4bd8a24a1449ac9d12b7c898
vishal3D <guptavishal049@gmail.com>
2025-05-28 16:14:14 +0530

    Added basic framework code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('submitFormDetails',()=>
+{
+        cy.get("#country").type("India")
+        cy.get(".suggestions ul li a").click()
+        cy.get(".btn-success").click()
+})
