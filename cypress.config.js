29b23f5f2df51fb39268d6a988442dc22ded4913
vishal3D <guptavishal049@gmail.com>
2025-05-30 17:41:27 +0530

    Added mochaAwesome reporting support

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 6000,
   watchForFileChanges :false,
+  reporter: 'cypress-mochawesome-reporter',
   env: {
     url: "https://rahulshettyacademy.com",
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
